d5e13c239a0ae17bfbaeacd8fe468ff476e1a4b7
imurata <61151479+imurata@users.noreply.github.com>
2023-10-18 19:47:26 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -x
 IMAGE_CLIENT=imuratashared/grpc-client
 IMAGE_SERVER=imuratashared/grpc-server
 
-kubectl create deploy --image $IMAGE grpc-server -r 1 --port 9000
+kubectl create deploy --image $IMAGE_SERVER grpc-server -r 1 --port 9000
 kubectl wait --timeout=30s --for=condition=available deploy grpc-server
 kubectl expose deploy grpc-server --port 9000
 kubectl run --image $IMAGE_CLIENT grpc-client -- --server grpc-server:9000
