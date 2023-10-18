d6df802be45c8439e1d20f1f3783a85f0f89ff2d
imurata <imurata@vmware.com>
2023-10-18 10:56:20 +0000

    nocomment

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ set -x
 IMAGE_CLIENT=imuratashared/grpc-client
 IMAGE_SERVER=imuratashared/grpc-server
 
-kubectl create deploy --image $IMAGE grpc-server -r 1 --port 9000
+kubectl create deploy --image $IMAGE_SERVER grpc-server -r 1 --port 9000
 kubectl wait --timeout=30s --for=condition=available deploy grpc-server
 kubectl expose deploy grpc-server --port 9000
-kubectl run --image $IMAGE_CLIENT grpc-client -- --server grpc-server:9000
-kubectl wait --timeout=30s --for=condition=Ready pod grpc-client
-kubectl expose pod grpc-client --port 8080
+kubectl create deploy --image $IMAGE_CLIENT grpc-client -r 1 --port 8080 -- ./client --server grpc-server:9000
+kubectl wait --timeout=30s --for=condition=available deploy grpc-client
+kubectl expose deploy grpc-client --port 8080
 
 
 
